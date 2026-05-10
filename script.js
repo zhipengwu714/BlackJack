11b315695e170ace6f547ddf057828eb9412752c
zhipengwu714 <zhipeng.wu44@bcmail.cuny.edu>
2026-05-10 02:40:10 -0400

    add buildDeck and shuffleDeck functions

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// We'll build these functions out on Day 2
-// For now, they just prove the buttons are wired up
+// ---- THE DECK ----
 
-function hit() {
-  document.getElementById("message").innerText = "You hit!";
-}
+const suits = ["♠", "♥", "♦", "♣"];
+const cardNames = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+const cardValues = [2,3,4,5,6,7,8,9,10,10,10,10,11];
 
-function stand() {
-  document.getElementById("message").innerText = "You stood!";
-}
+let deck = [];
+let playerHand = [];
+let dealerHand = [];
 
-function newGame() {
-  document.getElementById("message").innerText = "New game started!";
+function buildDeck() {
+  deck = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let c = 0; c < cardNames.length; c++) {
+      deck.push({
+        suit: suits[s],
+        name: cardNames[c],
+        value: cardValues[c]
+      });
+    }
+  }
 }
